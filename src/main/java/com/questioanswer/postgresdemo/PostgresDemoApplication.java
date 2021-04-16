f8d65d451bf929b3a554c03ba38c2ba3f2be616a
silas.dias <silasdjd@gmail.com>
2021-04-16 15:18:03 -0300

    Model AuditModel e Model Question

--- src/main/java/com/questioanswer/postgresdemo/PostgresDemoApplication.java
+++ src/main/java/com/questioanswer/postgresdemo/PostgresDemoApplication.java
@@ -2,8 +2,10 @@ package com.questioanswer.postgresdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PostgresDemoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/questioanswer/postgresdemo/model/Question.java
@@ -0,0 +1,24 @@
+package com.questioanswer.postgresdemo.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "questions")
+public class Question extends AuditModel{
+
+    @Id
+    @GeneratedValue(generator = "question_generator")
+    @SequenceGenerator(
+            name = "question_generator",
+            sequenceName = "question_sequence",
+            initialValue = 1000
+    )
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String title;
+
+    @Column(columnDefinition = "text")
+    private String description;
+}
