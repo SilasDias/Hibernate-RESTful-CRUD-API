c39e0ce42ee77e81a9281cfc72208a476c1adc0e
silas.dias <silasdjd@gmail.com>
2021-04-16 15:27:08 -0300

    Model Answer

---
+++ src/main/java/com/questioanswer/postgresdemo/model/Answer.java
@@ -0,0 +1,31 @@
+package com.questioanswer.postgresdemo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "answer")
+public class Answer extends AuditModel{
+
+    @Id
+    @GeneratedValue(generator = "answer_generator")
+    @SequenceGenerator(
+            name = "answer_generator",
+            sequenceName = "answer_sequence",
+            initialValue = 1000
+    )
+    private Long Id;
+
+    @Column(columnDefinition = "text")
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "question_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Question question;
+}
